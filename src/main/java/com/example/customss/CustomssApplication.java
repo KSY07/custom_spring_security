6339d8b7cde58a320a114c0fecefd570f79f9794
김세영 <ksy2008w@bizpeer.co.kr>
2023-04-04 11:17:14 +0900

    Project Init

---
+++ src/main/java/com/example/customss/CustomssApplication.java
@@ -0,0 +1,13 @@
+package com.example.customss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customss/CustomssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
